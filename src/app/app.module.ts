772f88e0bba34e8b004299e899c526ca505e072a
EdenFly66 <yerkoandresespinozasoto@gmail.com>
2022-09-26 20:13:23 -0300

    ingresar+registrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,15 @@ import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FuncionalidadesComponent } from './funcionalidades/funcionalidades.component';
 import { PieComponent } from './pie/pie.component';
 import { FiltroComponent } from './filtro/filtro.component';
+import { IngresarComponent } from './ingresar/ingresar.component';
+import { RegistrarComponent } from './registrar/registrar.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const appRoutes: Routes = [
+  {path:'',component:FuncionalidadesComponent},
+  {path:'ingresar',component:IngresarComponent},
+  {path:'registrar',component:RegistrarComponent}
+];
 
 @NgModule({
   declarations: [
@@ -16,11 +23,14 @@ import { FiltroComponent } from './filtro/filtro.component';
     CabeceraComponent,
     FuncionalidadesComponent,
     PieComponent,
-    FiltroComponent
+    FiltroComponent,
+    IngresarComponent,
+    RegistrarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -13,3 +13,4 @@ export class CabeceraComponent implements OnInit {
   }
 
 }
+
